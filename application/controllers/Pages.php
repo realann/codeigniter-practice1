a4a20be19531df4a25b393f7ef102bf2a4d5e4b8
realann <realnn0219@gmail.com>
2018-06-29 07:50:50 +0000

    Pagesコントローラのviewメソッドを次

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,6 +3,17 @@ class Pages extends CI_Controller {
 
        public function view($page = 'home')
        {
-           434343434343434343
+        if(!file_exists(APPPATH.'views/pages/'.$page.'.php'))
+        {
+            //Whoops,wedon'thaveapageforthat!
+            show_404();
+        }
+        
+        $data['title']=ucfirst($page);//Capitalizethefirstletter
+        
+        $this‐>load‐>view('templates/header',$data);
+        $this‐>load‐>view('pages/'.$page,$data);
+        $this‐>load‐>view('templates/footer',$data);
+         
        }
 }
